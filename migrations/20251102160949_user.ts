4f94c7f2af1c3909c4491127bda6b058ab7b5bff
Alexander Godwin <alecgee73@gmail.com>
2025-11-02 17:16:06 +0100

    user migration

--- migrations/20251102160949_user.ts
+++ migrations/20251102160949_user.ts
@@ -1,10 +1,21 @@
-import type { Knex } from "knex";
-
+import type { Knex } from "knex"
 
 export async function up(knex: Knex): Promise<void> {
+  return knex.schema.createTable("User", (table) => {
+    table.uuid("id").primary()
+    table.string("email", 255).notNullable().unique().index()
+    table.string("password", 255).nullable()
+    table.string("firstName", 100).nullable()
+    table.string("lastName", 100).nullable()
+    table.boolean("verified").defaultTo(false).notNullable()
+    table.string("resihubUserId", 255).nullable()
+    table.string("imageKey", 500).nullable()
+    table.string("imageUrl", 1000).nullable()
+    table.timestamp("urlExp").nullable()
+    table.timestamps(true, true)
+  })
 }
-
 
 export async function down(knex: Knex): Promise<void> {
+  return knex.schema.dropTableIfExists("User")
 }
-
